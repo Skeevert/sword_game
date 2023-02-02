503bfb558ea0652536baad37722f0a1dadab8d13
Dartstriker <25845169+Dartstriker@users.noreply.github.com>
2023-02-03 02:40:16 +0300

    Switched to Godot version 4.0 beta 17

--- Player.cs
+++ Player.cs
@@ -1,57 +1,56 @@
 using Godot;
 using System;
 
-public class Player : KinematicBody
+public partial class Player : CharacterBody3D
 {
 	public float MaxSpeed = 20F;
 	public float Acceleration = 200F;
 	public int FallAcceleration = 75;
 
 	private float _mouseSensitivity = 0.3F;
-	private Camera _camera;
-	
+	private Camera3D _camera;
+
 	// TODO: Move to separate Physics class
 	private float _friction = 6F;
-	
-	private Vector3 _velocity = Vector3.Zero;
-	
+
+	private Vector3 _velocity;
+
 	public override void _Ready()
 	{
-		_camera = GetNode<Camera>("Camera");
-		Input.SetMouseMode(Input.MouseMode.Captured);
+		_camera = GetNode<Camera3D>("Camera3D");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
 	public override void _Input(InputEvent inputEvent)
 	{
 		if (inputEvent is InputEventMouseMotion mouseMotionEvent)
 		{
-			RotateY(Mathf.Deg2Rad(-mouseMotionEvent.Relative.x * _mouseSensitivity));
-			
-			var change = Mathf.Deg2Rad(-mouseMotionEvent.Relative.y * _mouseSensitivity);
-			var futureCameraAngleV = change + _camera.Rotation.x;
+			RotateY(Mathf.DegToRad(-mouseMotionEvent.Relative.X * _mouseSensitivity));
+
+			var change = Mathf.DegToRad(-mouseMotionEvent.Relative.Y * _mouseSensitivity);
+			var futureCameraAngleV = change + _camera.Rotation.X;
 
 			// We should correct change in order not to overshoot [-90, 90] degrees.
 			// If everything is in limits, the expression simplifies to change = change.
 			// If not, change is corrected by the delta between overshot value and limits
-			change = change - (futureCameraAngleV - Mathf.Clamp(futureCameraAngleV, 
-				-Mathf.Pi / 2, 
+			change = change - (futureCameraAngleV - Mathf.Clamp(futureCameraAngleV,
+				-Mathf.Pi / 2,
 				Mathf.Pi / 2
 			));
-			
+
 			_camera.RotateX(change);
 		}
 	}
-	
-	public override void _PhysicsProcess(float delta)
-	{
-		var direction = Vector3.Zero;
-		
-		Vector3 rightAxis = GlobalTransform.basis.x;
-		Vector3 forwardAxis = GlobalTransform.basis.z;
-		
 
-		applyFriction(delta);
-		
+	public override void _PhysicsProcess(double doubleDelta)
+	{
+		float delta = (float)doubleDelta;
+
+		var direction = Vector3.Zero;
+
+		Vector3 rightAxis = GlobalTransform.Basis.X;
+		Vector3 forwardAxis = GlobalTransform.Basis.Z;
+
 		// TODO: Implement camera and then get its angle
 		if (Input.IsActionPressed("move_forward"))
 		{
@@ -69,42 +68,48 @@ public class Player : KinematicBody
 		{
 			direction -= rightAxis;
 		}
-		
+
 		if (direction != Vector3.Zero)
 		{
 			direction = direction.Normalized();
 		}
-		
-		var addVelocity = direction * Acceleration *delta;
-		
-		_velocity.x += addVelocity.x;
-		_velocity.z += addVelocity.z;
-		
+
+		_velocity = Velocity;
+
+		applyFriction(delta);
+
+		var addVelocity = direction * Acceleration * delta;
+
+		_velocity.X += addVelocity.X;
+		_velocity.Z += addVelocity.Z;
+
 		limitVelocity();
-	
+
 		// TODO: Implement jumps
-		_velocity.y = 0;
-		
-		_velocity = MoveAndSlide(_velocity, Vector3.Up);
+		_velocity.Y = 0;
+
+		Velocity = _velocity;
+		MoveAndSlide(); //since 4.0 MoveAndSlide now works with object properties (e.g. Velocity)
 	}
-	
+
 	private void applyFriction(float delta)
 	{
 		float speed = _velocity.Length();
-		
+
 		if (speed < 1)
 		{
-			_velocity.x = 0;
-			_velocity.z = 0;
+			_velocity.X = 0;
+			_velocity.Z = 0;
 			return;
 		}
-		
+
 		float multiplier = 1 - _friction * delta;
-		_velocity.x *= multiplier;
-		_velocity.z *= multiplier;
+		_velocity.X *= multiplier;
+		_velocity.Z *= multiplier;
 	}
-	
+
 	// Vector3.LimitLength() was added in 3.5, and we are working with 3.4
+	// Ok now we work with 4.0, consider changing
 	private void limitVelocity()
 	{
 		_velocity = _velocity.Length() > MaxSpeed ? _velocity.Normalized() * MaxSpeed : _velocity;
